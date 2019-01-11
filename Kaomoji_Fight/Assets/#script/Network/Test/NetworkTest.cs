d373a3c1e14169b44c64aa54f687fdefc845dd5c
HiromuHayasi <galaxy68xxx@gmail.com>
2019-01-11 18:10:22 +0900

    おぶじぇくとの、ざひょう・かいてん・すけーるがどうきするようになったよ

--- Kaomoji_Fight/Assets/#script/Network/Test/NetworkTest.cs
+++ Kaomoji_Fight/Assets/#script/Network/Test/NetworkTest.cs
@@ -3,78 +3,60 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NetworkTest : MonoBehaviour {
- //   #region
- //   // Use this for initialization
- //   void Start () {
- //       // Photonに接続する(引数でゲームのバージョンを指定できる)
- //       PhotonNetwork.ConnectUsingSettings(null);
-	//}
 
-
- //   // Auto-Join-Llobbyにチェックがついていると呼ばれないはず
- //   void OnConnectedToMaster()
- //   {
- //       Debug.Log("呼ばれちゃいけないんだけどなぁ・・・（´・ω・｀）");
- //   }
-
-
- //   // ロビーに入ると呼ばれる
- //   private void OnJoinedLobby()
- //   {
- //       Debug.Log("ロビーに入りました");
-
- //       // ルームに入室する
- //       PhotonNetwork.JoinRandomRoom();
- //   }
-
- //   // ルームに入室すると呼ばれる
-	//private void OnJoinedRoom()
- //   {
- //       Debug.Log("ルームへ入室しました");
- //   }
-
-
- //   // サーバーに到達できないと呼ばれる
- //   void OnFailedToConnectToPhoton(DisconnectCause cause)
- //   {
- //       Debug.Log("接続に失敗しとるやないかい！＼(゜ロ＼)(／ロ゜)／");
- //   }
-
-
- //   // ルーム入室に失敗すると呼ばれる
- //   private void OnPhotonRandomJoinFailed()
- //   {
- //       Debug.Log("ルームの入室に失敗しました");
-
- //       // ルームがないと入室に失敗するため、その時は自分で作る
- //       // 引数でルーム名を指定できる
- //       PhotonNetwork.CreateRoom("MyRoomName");
- //   }
- //   #endregion
-
+    // 生成するPrefabの場所
     private string m_resourcePath = "prefab/Test/Cube";
     [SerializeField]
     private float m_randomCircle = 4.0f;
 
     private const string ROOM_NAME = "RoomA";
+
+    // Test-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
     private bool m_fg = false;
+    GameObject obj;
+    // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
     void Start()
     {
+        // Photonに接続（引数にバージョンを入れる）
         PhotonNetwork.ConnectUsingSettings(null);
     }
 
     void OnJoinedLobby()
     {
         Debug.Log("ロビー入室");
-        PhotonNetwork.JoinOrCreateRoom(ROOM_NAME, new RoomOptions(), TypedLobby.Default);
+        // ルームを生成する
+        //PhotonNetwork.JoinOrCreateRoom(ROOM_NAME, new RoomOptions(), TypedLobby.Default);
+
+        /*ルームオプションの設定*/
+
+        // RoomOptionsのインスタンスを生成
+        RoomOptions roomOptions = new RoomOptions();
+
+        // ルームに入室できる最大人数。0を代入すると上限なし。
+        roomOptions.MaxPlayers = 4;
+
+        // ルームへの入室を許可するか否か
+        roomOptions.IsOpen = true;
+
+        // ロビーのルーム一覧にこのルームが表示されるか否か
+        roomOptions.IsVisible = true;
+
+        /* ルームの作成 */
+        /* ルームオプションを設定する場合、CreateRoomメソッドは引数3つのオーバーロードを使用します。 */
+
+        // 第1引数はルーム名、第2引数はルームオプション、第3引数はロビーです。
+        PhotonNetwork.JoinOrCreateRoom(ROOM_NAME, roomOptions, TypedLobby.Default);
     }
 
+
+    // オブジェクトの生成
     public void SpawnObject()
     {
-        PhotonNetwork.InstantiateSceneObject(m_resourcePath, GetRandomPosition(), Quaternion.identity, 0, null);
+        PhotonNetwork.Instantiate(m_resourcePath, GetRandomPosition(), Quaternion.identity, 0);
     }
 
+    // Vector3でランダムにポジションを取得
     private Vector3 GetRandomPosition()
     {
         Vector3 rand = Random.insideUnitCircle * m_randomCircle;
@@ -83,10 +65,47 @@ public class NetworkTest : MonoBehaviour {
 
     private void Update()
     {
+        // テストオブジェクトの生成
         if (Input.GetKey(KeyCode.Space) && !m_fg)
         {
             SpawnObject();
             m_fg = true;
+
+            obj = GameObject.Find("Cube(Clone)");            
+        }
+        // もう一個生成したいときに使う
+        if (Input.GetKey(KeyCode.M))
+        {
+            m_fg = false;
+        }
+        // 生成したオブジェクトを動かす
+        if (m_fg)
+        {
+            Move(obj);
+        }
+        
+    }
+
+    // 上下左右に動かすぜぃ( ･´ｰ･｀)ドヤァ
+    private void Move(GameObject obj)
+    {
+        Rigidbody rig = obj.GetComponent<Rigidbody>();
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rig.AddForce(new Vector3(0, .02f, 0) * 5f, ForceMode.Impulse);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rig.AddForce(new Vector3(0, -.02f, 0) * 5f, ForceMode.Impulse);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rig.AddForce(new Vector3(-.02f, 0, 0) * 5f, ForceMode.Impulse);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rig.AddForce(new Vector3(.02f, 0, 0) * 5f, ForceMode.Impulse);
         }
     }
 }
