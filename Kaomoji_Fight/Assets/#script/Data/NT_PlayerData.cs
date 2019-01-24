a876255100f6b1e1edb4d334190c9dd15c94cf04
motoya <hubuki008@gmail.com>
2019-01-24 12:50:09 +0900

    [ネットワークセレクト]確認画面完成

---
+++ Kaomoji_Fight/Assets/#script/Data/NT_PlayerData.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NT_PlayerData{
+
+    //プレイヤーデータ
+    private string name = "";
+    private Sprite Face;
+    private Color color;
+    private string SelectStage = "";
+
+    public static NT_PlayerData Instance
+    {
+        get;
+        private set;
+    }
+
+    public NT_PlayerData(string PlayerName = "むめい", Sprite PlayerFace = null, Color PlayerColor = new Color(), string stage = "stage1")
+    {
+        Instance = this;
+
+        name = PlayerName;
+        Face = PlayerFace;
+        color = PlayerColor;
+        SelectStage = stage;
+    }
+}
